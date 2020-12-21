6aabb7b2ca63d8d54f1d336d5c0a8e6ddc27e826
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2020-12-21 17:45:33 +0300

    Refactor everything

--- libraries/stdlib/js/src/kotlin/text/char.kt
+++ libraries/stdlib/js/src/kotlin/text/char.kt
@@ -177,7 +177,7 @@ public actual fun Char.isUpperCase(): Boolean {
     if (this < '\u0080') {
         return false
     }
-    return getCategoryValue() == CharCategory.UPPERCASE_LETTER.value
+    return isUpperCaseImpl()
 }
 
 /**
@@ -194,7 +194,7 @@ public actual fun Char.isLowerCase(): Boolean {
     if (this < '\u0080') {
         return false
     }
-    return getCategoryValue() == CharCategory.LOWERCASE_LETTER.value
+    return isLowerCaseImpl()
 }
 
 /**
