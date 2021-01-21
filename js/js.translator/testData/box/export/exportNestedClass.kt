97bfc052a2112a8020e722a5132913639d430695
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2021-01-21 13:03:56 +0300

    [JS IR] Add tests with export all file and export nested class

---
+++ js/js.translator/testData/box/export/exportNestedClass.kt
@@ -0,0 +1,23 @@
+// RUN_PLAIN_BOX_FUNCTION
+// INFER_MAIN_MODULE
+
+// MODULE: export-nested-class
+// FILE: lib.kt
+
+abstract class A {
+    abstract fun foo(k: String): String
+}
+
+@JsExport
+class B {
+    class Foo : A() {
+        override fun foo(k: String): String {
+            return "O" + k
+        }
+    }
+}
+
+// FILE: test.js
+function box() {
+    return new this["export-nested-class"].B.Foo().foo("K");
+}
